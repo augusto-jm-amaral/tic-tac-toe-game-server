0ecab24535da78c453f79652e91b6d7532a7531b
Augusto José <augusto.jm.amaral1@gmail.com>
2017-06-04 20:25:27 -0300

    Remove comments

--- db/index.js
+++ db/index.js
@@ -2,9 +2,7 @@ const redis   = require('./redis.js'),
       adapter = require('./redis-adapter.js');
 
 var init = () => {
-  // redis.client.flushall();
   redis.set('room_number', 0);
-  // redis.client.del('rooms');
   redis.client.del('rooms_wait');
 };
 
